f6bf02091217fdbb87ba50f3394ee0610390dac3
Joseph <josephajibodu@gmail.com>
2021-10-05 21:20:34 +0100

    🚧 Added posts markdown for the blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -9,12 +9,16 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm a proficient full stack developer with the aim of building you a smart and scalable digital solution. You can contact me on instagram <a href="http://www.instagram.com/cremirdevio">@cremirdevio</a></p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          I'm a proficient full stack developer with the aim of building you a
+          smart and scalable digital solution. You can contact me on instagram{" "}
+          <a target="_blank" href="https://www.instagram.com/cremirdevio">@cremirdevio</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
-  )
+  );
 }
